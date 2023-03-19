45dd23506a9645ee049eed1c95c478cc743ad729
Sarah Baughman <sarahbaughman92@gmail.com>
2023-03-19 10:48:32 -0400

    commit tree c8ab51fb178cd6d4ef407a97d2e5c8abc98b3b54

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+
+function scuberGreetingForFeet(ride){
+  if (ride <= 400) {
+    return 'This one is on me!'
+  } else if (ride < 2000) {
+    return 'That will be twenty bucks.'
+  /* } else if (feet > 2000) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (feet >= 2500) {
+    return 'No can do.'
+  }*/
+  }
 }
+  
+
 
 function ternaryCheckCity(){
   // Write your code here!
